98980377c464051e9c7f176a5baba249325b9490
OlivierMaria <olivier-maria@hotmail.com>
2023-03-22 00:13:25 +0100

    commit

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ApplicationRecord
+include PgSearch::Model
+pg_search_scope :search_full_text, against: {
+    title: 'A',
+    description: 'B',
+}
+    has_one_attached :photo
+    
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+
+belongs_to :category
+end
